50164b604001c84b2f842227cf9cf160aefecd0a
sorrowwolf <414471841@qq.com>
2022-03-20 19:19:26 +0800

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+ <template>
+   <div class="homepage-container">
+     <a-layout :style="{background: '#fff'}">
+      <a-layout-header class="header">
+        <div class="page-title">
+          慕课乐高
+        </div>
+      </a-layout-header>
+      <a-layout-content class="home-layout">
+
+      </a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+   </div>
+ </template>
