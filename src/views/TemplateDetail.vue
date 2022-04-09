b766a72cb2ebf3e572474d4f30311540c9197bf7
sorrowwolf <414471841@qq.com>
2022-04-09 20:33:37 +0800

    l-text

--- src/views/TemplateDetail.vue
+++ src/views/TemplateDetail.vue
@@ -39,7 +39,7 @@ import { defineComponent, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 import { GlobalDataProps } from '../store/index';
-import { TemplateProps } from '../store/index';
+import { TemplateProps } from '../store/templates';
 export default defineComponent({
   setup () {
     const route = useRoute();
